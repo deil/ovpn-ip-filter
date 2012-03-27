8ed6a1b43aea08419a125c13eb4e14d55399b5c5
Anton <deil@mneko.net>
2012-03-27 22:55:14 +0400

    added simple command-line arguments to import script

--- ovpn-ip-filter-import.rb
+++ ovpn-ip-filter-import.rb
@@ -1,13 +1,19 @@
 #!/usr/bin/ruby
 
-File.open("kusigma_vpn-status.txt", "r") do |f|
+input_file = ARGV.first
+default_mask = ARGV[1] || "32"
+
+puts "Parsing OpenVPN status file: %s" % input_file
+puts "Default netmask for incoming connections: %s" % default_mask
+
+File.open(input_file, "r") do |f|
  File.open("ovpn-ip-filter.txt", "w") do |w|
   while l = f.gets
    arr = l.split(',')
    if arr.first =~ /client/
     cn = arr[0]
     ip = arr[1].split(':').first
-    w.puts "%s %s/32" % [cn, ip]
+    w.puts "%s %s/%s" % [cn, ip, default_mask]
    end
   end
  end
